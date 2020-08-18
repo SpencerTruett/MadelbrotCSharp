d71d455cd115324fd2e47a9280d0ed99167c34b0
Spencer Truett <srtruett1993@gmail.com>
2020-08-18 13:13:27 -0500

    added ComplexNumber Class

--- Models/ComplexNumbers.cs
+++ Models/ComplexNumbers.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace MandelbrotCSharp.Models
 {
-    class ComplexNumbers
+    public class ComplexNumbers
     {
+        // real part
+        public double a;
+
+        // imaginary part
+        public double b;
+
+        public void Square()
+        {
+            double temp = (a * a) - (b * b);
+
+            b = 2.0 * a * b;
+            a = temp;
+        }
+
+        public double Magnitude()
+        {
+            return Math.Sqrt((a * a) + (b * b));
+        }
+
+        public void Add(ComplexNumbers c)
+        {
+            a += c.a;
+            b += c.b;
+        }
     }
 }
